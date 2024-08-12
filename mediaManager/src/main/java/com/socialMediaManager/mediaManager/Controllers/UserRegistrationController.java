61d00308b06a231604d13041324dceb8d0fbee71
himanshu.ranjan <himanshu.ranjan@cashfree.com>
2024-08-12 16:53:06 +0530

    dummy commit

--- mediaManager/src/main/java/com/socialMediaManager/mediaManager/Controllers/UserRegistrationController.java
+++ mediaManager/src/main/java/com/socialMediaManager/mediaManager/Controllers/UserRegistrationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserRegistrationController {
+public class UserRegistrationController  {
     @Autowired
     private TwitterServiceImpl twitterService;
     @PostMapping("/user-registartion")
